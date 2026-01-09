27716827089ec7e59ca303382cf64ce0a74fb8b4
Niklas V <niklas.vilnersson@gmail.com>
2026-01-09 14:45:49 +0100

    chore: semantic-release update

--- release.config.cjs
+++ release.config.cjs
@@ -126,7 +126,7 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "jq '.version = \"${nextRelease.version}\"' custom_components/met_rain_risk/manifest.json > manifest.tmp && mv manifest.tmp custom_components/met_rain_risk/manifest.json && cd custom_components && zip -r met_rain_risk.zip met_rain_risk",
+          "node .release/update-manifest-version.js --file custom_components/met_rain_risk/manifest.json --version \"${nextRelease.version}\" && (cd custom_components/met_rain_risk && rm -f ../met_rain_risk.zip && zip -r ../met_rain_risk.zip . -x \"__pycache__/*\" \"*.pyc\" \".DS_Store\" \".pycacheprefix/*\" \".pytest_cache/*\" \".mypy_cache/*\")",
 
         // After a successful release, comment on issues referenced via "Fixes #123" etc
         // in commits included in this release. GitHub will still close issues automatically
