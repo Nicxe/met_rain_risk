234d4c392870c9b0750d8885ee1dfe1d7f00559b
Niklas V <niklas.vilnersson@gmail.com>
2026-03-05 09:20:13 +0100

    fix: prevent semantic-release success step from failing (#30)

--- release.config.cjs
+++ release.config.cjs
@@ -1,6 +1,17 @@
-module.exports = require("@nicxe/semantic-release-config")({
+const config = require("@nicxe/semantic-release-config")({
   componentDir: "custom_components/met_rain_risk",
   manifestPath: "custom_components/met_rain_risk/manifest.json",
   projectName: "MET Rain Risk",
   repoSlug: "Nicxe/met_rain_risk"
-});
+}
+);
+
+const githubPlugin = config.plugins.find(
+  (plugin) => Array.isArray(plugin) && plugin[0] === "@semantic-release/github"
+);
+
+if (githubPlugin?.[1]) {
+  githubPlugin[1].successCommentCondition = false;
+}
+
+module.exports = config;
